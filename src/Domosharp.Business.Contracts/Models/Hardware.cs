920c4df9afa09196e56384a87b31a9400c86bf9f
Cyril <brunet.cyril@hotmail.fr>
2024-08-23 18:47:54 +0200

    Testing app instead of build

--- src/Domosharp.Business.Contracts/Models/Hardware.cs
+++ src/Domosharp.Business.Contracts/Models/Hardware.cs
@@ -4,6 +4,11 @@ namespace Domosharp.Business.Contracts.Models
 {
   public class Hardware : IHardware
   {
+    public Hardware()
+    {
+      Name = string.Empty;
+    }
+
     public int Id { get; set; }
     public string Name { get; set; }
     public bool Enabled { get; set; }
